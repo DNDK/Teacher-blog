465b764622ef850915c0fcca489c5cb182821984
DNDK <59180060+DNDK@users.noreply.github.com>
2020-05-15 16:32:35 +0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kc&&bb_z+@7zlkyc(5)x$9bf#coc0fcw=59^nrt-41(decn2fx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
